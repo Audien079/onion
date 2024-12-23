07e4034db36a19bd84e6730f120018d4dad25e93
audienio <max@audien.io>
2024-12-23 16:36:09 +0530

    Added check for title using beautifulsoup

--- report/tasks.py
+++ report/tasks.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+from bs4 import BeautifulSoup
 from admin import logger
 from celery import shared_task
 from report.models import Website
@@ -12,13 +13,21 @@ def check_status():
     Checks status of all websites
     """
     websites = Website.objects.all()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
 
     for website in websites:
-        response = requests.get(website, timeout=5)
+        response = requests.get(website, headers=headers, timeout=7)
         status_code = response.status_code
         if status_code == 200:
-            website.status = True
-            website.last_active = datetime.datetime.now()
+            soup = BeautifulSoup(response.content, 'html.parser')
+            title = soup.title.string if soup.title else "No title found"
+            if "error" not in title:
+                website.status = True
+                website.last_active = datetime.datetime.now()
+            else:
+                website.status = False
         else:
             website.status = False
 
